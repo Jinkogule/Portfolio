97f35b0753136c2284a3b6a82b3743a433cd9836
Jinkogule <lucaspimenta21@gmail.com>
2024-03-28 17:47:55 -0300

    Atualização de dados do projeto  filhos do coração

--- controllers/HomeController.go
+++ controllers/HomeController.go
@@ -323,34 +323,6 @@ func (c *HomeController) Get() {
 				Titulo:   "Contexto do Projeto",
 				Conteudo:   "Este projeto foi desenvolvido como parte do trabalho para a disciplina Programação Orientada a Objetos, ministrada pela Professora Vânia Neves no Instituto de Computação da Universidade Federal Fluminense, durante o período 2º/2020. O projeto foi realizado por Gabriel Barbedo, Lucas Couto e Lucas Pimenta.",
 			},
-			{
-				Titulo:   "Contexto do Projeto",
-				Conteudo:   "Este projeto foi desenvolvido como parte do trabalho para a disciplina Programação Orientada a Objetos, ministrada pela Professora Vânia Neves no Instituto de Computação da Universidade Federal Fluminense, durante o período 2º/2020. O projeto foi realizado por Gabriel Barbedo, Lucas Couto e Lucas Pimenta.",
-			},
-			{
-				Titulo:   "Contexto do Projeto",
-				Conteudo:   "Este projeto foi desenvolvido como parte do trabalho para a disciplina Programação Orientada a Objetos, ministrada pela Professora Vânia Neves no Instituto de Computação da Universidade Federal Fluminense, durante o período 2º/2020. O projeto foi realizado por Gabriel Barbedo, Lucas Couto e Lucas Pimenta.",
-			},
-			{
-				Titulo:   "Contexto do Projeto",
-				Conteudo:   "Este projeto foi desenvolvido como parte do trabalho para a disciplina Programação Orientada a Objetos, ministrada pela Professora Vânia Neves no Instituto de Computação da Universidade Federal Fluminense, durante o período 2º/2020. O projeto foi realizado por Gabriel Barbedo, Lucas Couto e Lucas Pimenta.",
-			},
-			{
-				Titulo:   "Contexto do Projeto",
-				Conteudo:   "Este projeto foi desenvolvido como parte do trabalho para a disciplina Programação Orientada a Objetos, ministrada pela Professora Vânia Neves no Instituto de Computação da Universidade Federal Fluminense, durante o período 2º/2020. O projeto foi realizado por Gabriel Barbedo, Lucas Couto e Lucas Pimenta.",
-			},
-			{
-				Titulo:   "Contexto do Projeto",
-				Conteudo:   "Este projeto foi desenvolvido como parte do trabalho para a disciplina Programação Orientada a Objetos, ministrada pela Professora Vânia Neves no Instituto de Computação da Universidade Federal Fluminense, durante o período 2º/2020. O projeto foi realizado por Gabriel Barbedo, Lucas Couto e Lucas Pimenta.",
-			},
-			{
-				Titulo:   "Contexto do Projeto",
-				Conteudo:   "Este projeto foi desenvolvido como parte do trabalho para a disciplina Programação Orientada a Objetos, ministrada pela Professora Vânia Neves no Instituto de Computação da Universidade Federal Fluminense, durante o período 2º/2020. O projeto foi realizado por Gabriel Barbedo, Lucas Couto e Lucas Pimenta.",
-			},
-			{
-				Titulo:   "Contexto do Projeto",
-				Conteudo:   "Este projeto foi desenvolvido como parte do trabalho para a disciplina Programação Orientada a Objetos, ministrada pela Professora Vânia Neves no Instituto de Computação da Universidade Federal Fluminense, durante o período 2º/2020. O projeto foi realizado por Gabriel Barbedo, Lucas Couto e Lucas Pimenta.",
-			},
 			
 		},
 		Status:             "Concluído",
@@ -426,8 +398,13 @@ func (c *HomeController) Get() {
 		DescricaoSimples: "Site desenvolvido para a disciplina Gerência de Projeto e Manutenção de Software.",
 		DescricaoDetalhada: []TopicoDeProjeto{
 			{
-				Titulo :   "Visão Geral",
-				Conteudo:   "ghrhrr",
+				Titulo: "Sobre o Filhos do Coração",
+				Conteudo: "O “Filhos do Coração” é um software desenvolvido para auxiliar no processo de adoção de crianças. Ele permite gerenciar desde a disponibilização de informações gerais sobre a adoção, incluindo textos e imagens de profissionais da área, depoimentos e contatos, até o registro da adoção em si. O sistema apresenta aos candidatos o processo de adoção, detalhando as etapas e prazos, e informa toda a documentação necessária. Além disso, permite que os usuários tirem dúvidas sobre o processo e a documentação.<br><br>O software apresenta casos de sucesso de adoções de crianças e permite que os usuários verifiquem informações e imagens sobre as crianças disponíveis para adoção, incluindo nome, idade, localização, estado de saúde, fotos e vídeos. O sistema verifica as informações dos candidatos a pais e permite que eles selecionem e adotem uma criança. Em caso de uma adoção não funcionar bem para os pais ou a criança, o sistema permite o cancelamento da adoção. Além disso, possibilita a adoção de famílias de dois ou mais irmãos pelos mesmos pais.<br><br>O projeto “Filhos do Coração” foi desenvolvido utilizando a linguagem de programação PHP com o framework Laravel e o banco de dados PostgreSQL. Ele também permite que os usuários enviem imagens das famílias, utilizando o S3 da Amazon para armazená-las.",
+			},
+			
+			{
+				Titulo :   "Contexto Acadêmico",
+				Conteudo:   "Este projeto foi desenvolvido como parte da disciplina de Gerência de Projeto e Manutenção de Software, tendo como principal objetivo a elaboração de um relatório detalhado incluindo o plano de comunicação, o plano de riscos, as estratégias de monitoramento e controle, e o plano de gerência e configuração utilizados no projeto. Além disso, o relatório também destacou as lições aprendidas e as inovações implementadas que foram além do escopo original do projeto.",
 			},
 			
 		},
